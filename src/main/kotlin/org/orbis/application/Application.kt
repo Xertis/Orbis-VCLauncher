62374c7a9a4fc776694d86fdd0e2edad573de5e3
Xertis <118364459+Xertis@users.noreply.github.com>
2025-12-08 02:25:50 +0300

    added icon

--- src/main/kotlin/org/orbis/application/Application.kt
+++ src/main/kotlin/org/orbis/application/Application.kt
@@ -5,14 +5,21 @@ import javafx.fxml.FXMLLoader
 import javafx.scene.Scene
 import javafx.stage.Stage
 import javafx.scene.text.Font
+import javafx.scene.image.Image
 
 class Orbis : Application() {
     override fun start(stage: Stage) {
-
         Font.loadFont(Orbis::class.java.getResourceAsStream("/org/orbis/application/fonts/ithaca.ttf"), 18.0)
 
         val fxmlLoader = FXMLLoader(Orbis::class.java.getResource("main.fxml"))
         val scene = Scene(fxmlLoader.load(), 700.0, 400.0)
+        try {
+            val icon = Image(Orbis::class.java.getResourceAsStream("/org/orbis/application/icon.png"))
+
+            stage.icons.add(icon)
+        } catch (e: Exception) {
+            System.err.println("Ошибка загрузки иконки: ${e.message}")
+        }
 
         stage.title = "Orbis"
         stage.scene = scene
